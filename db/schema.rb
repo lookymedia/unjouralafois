5f53b9d44f86c2de5eb025c27882cdeb30affae5
Nicolas Pellerin <nickypellerin@gmail.com>
2016-03-06 03:15:02 -0500

    Update admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305161256) do
+ActiveRecord::Schema.define(version: 20160306072349) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160305161256) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
