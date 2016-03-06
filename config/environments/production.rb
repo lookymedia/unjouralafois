dc1beb5e81ec2ce25c4ec115890472a729a5a013
Nicolas Pellerin <nickypellerin@gmail.com>
2016-03-06 12:06:50 -0500

    oufff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
 config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region=> 'us-east-1',
+  :s3_region=> 'us-standard',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
