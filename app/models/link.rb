e54a07ecf9bbd9d58f2b1a7102015548dc0d5e94
Nicolas Pellerin <nickypellerin@gmail.com>
2016-03-06 14:08:59 -0500

    date add

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,7 @@ class Link < ActiveRecord::Base
 	has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+    validates_attachment_size :image, :in => 0.megabytes..2.megabytes
  
 
 end
