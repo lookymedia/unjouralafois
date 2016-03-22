3675be62748f92e729fd2abf69795a63b6c83d79
Nicolas Pellerin <nickypellerin@gmail.com>
2016-03-22 10:20:07 -0400

    mobile thumb photos

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
-	has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.png"
+	has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing_:style.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
     validates_attachment_size :image, :in => 0.megabytes..2.megabytes
